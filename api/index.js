ed2cf0312b7ee1a77fc88be8faf74dbfd41153ca
crichdlive2026 <nareshnangad@gmail.com>
2026-06-21 22:05:18 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
 
   // यहाँ आपको हर मैच से पहले नया m3u8 लिंक डालना होगा
-  const liveMatchLink = "https://in-mc-flive.fancode.com/mumbai/143418_english_hls_ce2bd0636688281_1ta-di_h264/index.m3u8";
+  const liveMatchLink = "https://hesads.akamaized.net/ns/1780825385600/50a88b5e4cf89c2d388dc64696040f0f/master.m3u8";
 
   res.status(200).json({ 
       success: true, 
