86c77875d85cd00fef13912314722ce02bb62124
crichdlive2026 <nareshnangad@gmail.com>
2026-06-30 19:59:34 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ module.exports = async function(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
 
   // लिंक बिल्कुल एक ही लाइन में होना चाहिए
-  const liveMatchLink = "https://hesads.akamaized.net/ns/1781699063750/356365b55e996e2e86f0407c31e92500/master.m3u8";
+  const liveMatchLink = "https://amg01269-amg01269c1-sportstribal-emea-5204.playouts.now.amagi.tv/playlist/amg01269-willowtvfast-willowplus-sportstribalemea/playlist.m3u8";
   // रिस्पॉन्स भेजना
   res.status(200).json({ 
       success: true, 
