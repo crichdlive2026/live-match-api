a74d0247080006b7ef8fa2ddf242dd1ec8c7afb7
crichdlive2026 <nareshnangad@gmail.com>
2026-06-30 16:43:25 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,8 +4,7 @@ module.exports = async function(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
 
   // लिंक बिल्कुल एक ही लाइन में होना चाहिए
-  const liveMatchLink = "https://crickcast-fam.pages.dev/api/proxy?u=https%3A%2F%2Fin-mc-fblive.fancode.com%2Fmumbai%2F143399_english_hls_b76266f66a18138_1ta-di_h264%2F720p.m3u8";
-
+  const liveMatchLink = "https://crickcast-fam.pages.dev/api/proxy?u=https%3A%2F%2Fin-mc-fblive.fancode.com%2Fmumbai%2F143399_english_hls_b76266f66a18138_1ta-di_h264%2Findex.m3u8";
   // रिस्पॉन्स भेजना
   res.status(200).json({ 
       success: true, 
