e1551d6cdf6ecc5dc379630b25da17afaaf2d8b8
crichdlive2026 <nareshnangad@gmail.com>
2026-06-30 15:18:36 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,8 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
 
   // यहाँ आपको हर मैच से पहले नया m3u8 लिंक डालना होगा
-  const liveMatchLink = "https://crickcast-fam.pages.dev/fc-play?url=https%3A%2F%2Fin-mc-fblive.fancode.com%2Fmumbai%2F143399_english_hls_b76266f66a18138_1ta-di_h264%2Findex.m3u8";
+  const liveMatchLink = "https://crickcast-fam.pages.dev/api/proxy?u=https%3A%2F%2Fin-mc-fblive.fancode.com%2Fmumbai%2F143399_english_hls_b76266f66a18138_1ta-di_h264%2F720p.m3u8
+";
 
   res.status(200).json({ 
       success: true, 
