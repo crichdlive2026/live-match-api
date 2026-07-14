4beceb7c7d95be5386f1d6f98c1f28de0ed51a1e
crichdlive2026 <nareshnangad@gmail.com>
2026-07-14 15:23:55 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ module.exports = async function(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
 
   // लिंक बिल्कुल एक ही लाइन में होना चाहिए
-  const liveMatchLink = "https://dishmt.slivcdn.com/hls/live/2020591-b/TEN3HD/hdntl=exp=1784105496~acl=%2f*~id=d05a3315f69543e78a4f1d80b9ca0f1d-1784018925346~data=hdntl~hmac=a2a11bd20b19522c244f33bbcf3080e2aaf8fd8fa6a9a6aef5472d8c0cc5f265/master_3500.m3u8";
+  const liveMatchLink = "https://203-137-131-7.twitcasting.tv/tc.livehls/v1/streams/838127035/hls/1008.96/media.m3u8?useQSS=1";
   // रिस्पॉन्स भेजना
   res.status(200).json({ 
       success: true, 
