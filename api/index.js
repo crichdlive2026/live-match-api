1dc6ec109ea519ee09e8603db4c4c422e404ae0c
crichdlive2026 <nareshnangad@gmail.com>
2026-07-19 15:20:09 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ module.exports = async function(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
 
   // लिंक बिल्कुल एक ही लाइन में होना चाहिए
-  const liveMatchLink = "https://hugh.cdn.rumble.cloud/live/mu10xuf3/slot-37/zta4-vncc/chunklist.m3u8";
+  const liveMatchLink = "https://dishmt.slivcdn.com/hls/live/2020591/TEN3HD/hdntl=exp=1784269824~acl=%2f*~id=569eecec-a4fc-49e6-b571-8b57995a2196~data=hdntl~hmac=1a48f391de42c32afc9bdc1f42d7da1ea3849b181d048accb56a1f3db7641e2c/master_3500.m3u8";
   // रिस्पॉन्स भेजना
   res.status(200).json({ 
       success: true, 
