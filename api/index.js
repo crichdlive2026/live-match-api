476ed78cbf25c7f3635b1eb9583d5adecd979ce6
crichdlive2026 <nareshnangad@gmail.com>
2026-07-19 20:13:49 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,10 @@
 module.exports = async function(req, res) {
-  // CORS Headers (ताकि वेबसाइट इसे पढ़ सके)
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
 
-  // आपका SonyLIV Ten 3 HD का असली m3u8 लिंक
+  // इस लाइन के अंदर अपना नया m3u8 लिंक पेस्ट करें
   const liveMatchLink = "https://stream.ottplus.live/live/ten_1_hd_abr/live/ten_1_hd_720/chunks.m3u8";
 
-  // रिस्पॉन्स भेजना
   res.status(200).json({ 
       success: true, 
       live_link: liveMatchLink 
