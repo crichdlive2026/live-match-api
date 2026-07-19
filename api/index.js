dae285ae8df27f10d7bd88cb93b616e5b99fb2cb
crichdlive2026 <nareshnangad@gmail.com>
2026-07-19 15:25:06 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ module.exports = async function(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
 
   // लिंक बिल्कुल एक ही लाइन में होना चाहिए
-  const liveMatchLink = "https://dishmt.slivcdn.com/hls/live/2020591/TEN3HD/hdntl=exp=1784269824~acl=%2f*~id=569eecec-a4fc-49e6-b571-8b57995a2196~data=hdntl~hmac=1a48f391de42c32afc9bdc1f42d7da1ea3849b181d048accb56a1f3db7641e2c/master_3500.m3u8";
+  const liveMatchLink = "https://dishmt.slivcdn.com/hls/live/2020591/TEN3HD/hdntl=exp=1784529012~acl=/*~id=1fee2002-e826-4dff-a118-acac0deb270a~data=hdntl~hmac=25fcc4cbee06600f79c868d9220d5b9cc9557d83c7281408547f7c5dfaff676b/master_3500.m3u8";
   // रिस्पॉन्स भेजना
   res.status(200).json({ 
       success: true, 
