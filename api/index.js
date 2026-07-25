adbe2cd910416f8af5a1fe980de8041a2f52543e
crichdlive2026 <nareshnangad@gmail.com>
2026-07-25 17:24:38 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ module.exports = async function(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
 
   // इस लाइन के अंदर अपना नया m3u8 लिंक पेस्ट करें
-  const liveMatchLink = "https://stream.ottplus.live/live/ten_1_hd_abr/live/ten_1_hd_720/chunks.m3u8";
+  const liveMatchLink = "https://bpl24middleeast.akamaized.net/hls/live/2109311/j15klM//level_0.m3u8";
 
   res.status(200).json({ 
       success: true, 
