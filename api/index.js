ebaadf33c800556ba978c0b130a363821d8f6ca9
crichdlive2026 <nareshnangad@gmail.com>
2026-08-08 10:45:49 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ module.exports = async function(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
 
   // इस लाइन के अंदर अपना नया m3u8 लिंक पेस्ट करें
-  const liveMatchLink = "https://dishmt.slivcdn.com/hls/live/2020591/TEN3HD/hdntl=exp=1786220671~acl=/*~id=158e8351-3529-4a2f-8dbc-a875e210522e~data=hdntl~hmac=419cc06abe626aa97c516e738d951497f2b5ad11e1f1d952f7886072a907b0e7/master_664.m3u8?CMCD=cid%3D%22a225f9ebe072%22%2Cmtp%3D500%2Cot%3Dm%2Csf%3Dh%2Csid%3D%22dcbb7d1a-b7a7-4724-93a5-fd3727ccdad3%22%2Csu&CMCD=cid%3D%22a225f9ebe072%22%2Cmtp%3D15600%2Cot%3Dm%2Csf%3Dh%2Csid%3D%22dcbb7d1a-b7a7-4724-93a5-fd3727ccdad3%22";
+  const liveMatchLink = "https://dishmt.slivcdn.com/hls/live/2011747/TEN1HD/hdntl=exp=1786220671~acl=/*~id=158e8351-3529-4a2f-8dbc-a875e210522e~data=hdntl~hmac=419cc06abe626aa97c516e738d951497f2b5ad11e1f1d952f7886072a907b0e7/master_664.m3u8?CMCD=cid%3D%2223efd562d926%22%2Cmtp%3D500%2Cot%3Dm%2Csf%3Dh%2Csid%3D%225946a663-2a95-4cdc-9e40-33598f522af8%22%2Csu&CMCD=cid%3D%2223efd562d926%22%2Cmtp%3D12500%2Cot%3Dm%2Csf%3Dh%2Csid%3D%225946a663-2a95-4cdc-9e40-33598f522af8%22";
 
   res.status(200).json({ 
       success: true, 
