87bfabe4d17780a713e39fb8b422c02de0468ba7
crichdlive2026 <nareshnangad@gmail.com>
2026-08-09 20:01:29 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ module.exports = async function(req, res) {
   res.setHeader('Content-Type', 'application/json');
 
   // यहाँ सिर्फ अपना .m3u8 लिंक डालें
-  const liveContent = "http://172.105.41.183:8080/live/naresh.m3u8";
+  const liveContent = "https://172.105.41.183:8080/live/naresh.m3u8";
 
   res.status(200).json({ 
       success: true, 
