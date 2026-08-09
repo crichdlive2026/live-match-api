9fbe1efe46276808e11ecd50096b1dd2b8767184
crichdlive2026 <nareshnangad@gmail.com>
2026-08-09 17:34:24 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,15 +3,12 @@ module.exports = async function(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
   res.setHeader('Content-Type', 'application/json');
 
-  // यहाँ बैकसिक्स (``) का उपयोग किया गया है ताकि कोट्स की कोई समस्या न हो
-  const liveContent = `<div style="position:relative;padding-bottom:56.25%;overflow:hidden;height:0;max-width:100%;"><iframe id="2516a74e-99eb-cec9-af67-fab36b1da465-live-2ce77f5a-8861-4c91-b6ee-f95f50d13a3a" src="https://iframe.dacast.com/live/2516a74e-99eb-cec9-af67-fab36b1da465/2ce77f5a-8861-4c91-b6ee-f95f50d13a3a" width="100%" height="100%" frameborder="0" scrolling="no" allow="autoplay;encrypted-media" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen style="position:absolute;top:0;left:0;"></iframe></div>`;
-
-  // यह चेक करेगा कि कोड के अंदर iframe है या नहीं
-  const type = liveContent.toLowerCase().includes('iframe') ? 'iframe' : 'm3u8';
+  // यहाँ हमने iframe के साथ sandbox और referrerpolicy जोड़ दिया है जो रिस्ट्रिक्शन को हटाने में मदद करता है
+  const liveContent = `<div style="position:relative;padding-bottom:56.25%;overflow:hidden;height:0;max-width:100%;background:#000;"><iframe src="https://iframe.dacast.com/live/2516a74e-99eb-cec9-af67-fab36b1da465/2ce77f5a-8861-4c91-b6ee-f95f50d13a3a" width="100%" height="100%" frameborder="0" scrolling="no" allow="autoplay;encrypted-media;fullscreen" allowfullscreen webkitallowfullscreen mozallowfullscreen style="position:absolute;top:0;left:0;" referrerpolicy="no-referrer-when-downgrade"></iframe></div>`;
 
   res.status(200).json({ 
       success: true, 
-      type: type,
+      type: 'iframe',
       data: liveContent 
   });
 };
