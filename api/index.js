ec30ad15f15d80f73f0a43981cb7da40144e1ec5
crichdlive2026 <nareshnangad@gmail.com>
2026-08-24 20:37:54 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ module.exports = async function(req, res) {
   }
 
   // 👇 आपका नया लाइव मैच का M3U8 लिंक 👇
-  const liveContent = "https://7nyaler.streamhostingcdn.top/stream/10/index.m3u8";
+  const liveContent = "https://stream.ottplus.live/live/ten_cricket_hd_abr/live/ten_cricket_hd_720/chunks.m3u8";
 
   res.status(200).send(JSON.stringify({ 
       success: true, 
