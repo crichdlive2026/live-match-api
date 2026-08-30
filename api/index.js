c10e02067c1c10fe2b010ef65a40fcf2b435e7a4
crichdlive2026 <nareshnangad@gmail.com>
2026-08-30 14:40:20 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,7 +13,8 @@ module.exports = async function(req, res) {
   }
 
   // 👇 आपका नया लाइव मैच का M3U8 लिंक 👇
-  const liveContent = "https://stream.ottplus.live/live/ten_cricket_hd_abr/live/ten_cricket_hd_720/chunks.m3u8";
+  const liveContent = "https://dishmt.slivcdn.com/hls/live/2020592/TEN3SD/hdntl=exp=1788163252~acl=/*~id=a9c4449d-9503-4312-9096-bac90ec42129~data=hdntl~hmac=e047ff5ef2fd349cde2a7acc0f928304a48a878f96c35f4d360059525349cfb4/master_412.m3u8?CMCD=cid%3D%22d273fef0cec7%22%2Cmtp%3D500%2Cot%3Dm%2Csf%3Dh%2Csid%3D%226c9a775a-53a4-4b9d-bb14-d963680f28f9%22%2Csu&CMCD=cid%3D%22d273fef0cec7%22%2Cmtp%3D7000%2Cot%3Dm%2Csf%3Dh%2Csid%3D%226c9a775a-53a4-4b9d-bb14-d963680f28f9%22
+";
 
   res.status(200).send(JSON.stringify({ 
       success: true, 
